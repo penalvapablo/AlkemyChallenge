051dd52772adbf2e618405480e3b936dd4d3fc66
Pablo Penalva <penalva.pablo@gmail.com>
2022-03-25 10:52:50 -0300

    signup email changes

--- src/components/users/utils/signUpEmail.js
+++ src/components/users/utils/signUpEmail.js
@@ -1,12 +1,15 @@
 import sgMail from '@sendgrid/mail';
 import config from '../../../config/index.js';
 
+console.log(config.SENDGRID_API_KEY)
+console.log(config.SENDGRID_EMAIL)
+
 sgMail.setApiKey(config.SENDGRID_API_KEY);
 export default async function signUpEmail(email) {
   try {
     const message = {
       to: `${email}`,
-      from: 'penalva.estudio@gmail.com',
+      from: `${config.SENDGRID_EMAIL}`,
       subject: 'Bienvenido a Disney',
       text: 'bienvenido',
       html: '<h1>Bienvenido</h1>',
